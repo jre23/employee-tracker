054346c0d5f1801841cc997a855ed3df04fb2a7f
Joel Estrada <jre.estrada@gmail.com>
2020-11-20 20:30:58 -0800

    added crud.js file to connect to mysql database, also npm init, npm i mysql, and npm i inquirer

---
+++ Assets/employeeManageCrud.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "employee_managementDB"
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log("Connection successful!");
+});
